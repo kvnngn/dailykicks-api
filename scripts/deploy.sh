ab50b950586f0b0a4d2d1cb2b03cd872a8ac68fc
Nguyen Kevin <kevin.nguyen@grumatic.com>
2022-06-25 19:02:25 +0900

    ahh deploy file

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+echo "Kill all the running PM2 actions"
+sudo pm2 kill
+
+echo "Update app from Git"
+git pull
+
+# echo "Install app dependencies"
+# sudo rm -rf node_modules package-lock.json
+# sudo npm install
+
+echo "Build your app"
+sudo npm run build
+
+echo "Run new PM2 action"
+sudo pm2 start ecosystem.json
