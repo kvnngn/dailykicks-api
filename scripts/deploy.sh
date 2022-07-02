36071442e2521a4e8bbdf5e8ee3d106938b9d10f
Nguyen Kevin <kevin.nguyen@grumatic.com>
2022-07-02 17:32:57 +0900

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,12 @@ sudo pm2 kill
 echo "Update app from Git"
 git pull
 
-# echo "Install app dependencies"
+echo "Install app dependencies"
 # sudo rm -rf node_modules package-lock.json
-# sudo npm install
+sudo npm install
 
-echo "Build your app"
-sudo npm run build
+# echo "Build your app"
+# sudo npm run build
 
 echo "Run new PM2 action"
 sudo pm2 start "npm run start" --name dailykicks-api 
